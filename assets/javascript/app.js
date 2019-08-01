839210ff5f315c7ea5c816b63720d3939c2cc9f4
Jesse Denick <jessedenick@gmail.com>
2019-08-01 10:53:06 -0400

    added results page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-alert("Testing")
+alert("Reset")
 $('#start').on("click",function(){
     game.start();
 })
@@ -16,24 +16,52 @@ var questions = [{
 var game = {
     correct: 0,
     incorrect: 0,
-    counter: 120,
+    counter: 10,
     countdown: function(){
         game.counter--;
         $('#counter').html(game.counter);
-        if(game.counter<=0){
+        if(game.counter<1){
             console.log("Time is up!");
-            game.done()
+            game.done();
         }
     },
+
     start: function(){
-        timer = setInterval(game.countdown,1000);
-        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>');
+        timer = setInterval(game.countdown, 1000);
+        $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">10</span> Seconds</h2>');
         $('#start').remove();
         for(var i=0; i<questions.length; i++){
             $('#subwrapper').append('<h2>'+questions[i].question+'</h2>');
             for(var j=0; j<questions[i].answers.length;j++){
-                $("#subwrapper").append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j])
+                $("#subwrapper").append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j]);
             }
         }
-    }
+    },
+
+    done: function(){
+        $.each($("input[name='question-0'];checked"),function(){
+            if($(this).val() == questions[0].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+        $.each($('input[name="question-1]";checked]'),function(){
+            if($(this).val() == questions[1].correctAnswer){
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        });
+
+        this.result();
+        },
+        result: function(){
+            clearInterval(timer);
+            $('#subwrapper h2').remove();
+            $('#subwrapper').html("<h2>Time is up!!!</h2>");
+            $('#subwrapper').append("<h3>Correct Answers: "+this.correct+"</h3>");
+            $('#subwrapper').append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
+            $('#subwrapper').append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</h3>");
+        }
 }
